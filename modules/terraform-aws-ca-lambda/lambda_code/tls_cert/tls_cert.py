4a896177b8aa7c37a267c61359fd89f3dca62eaa
Paul Schwarzenberger <paul@celidor.net>
2024-02-02 08:49:04 +0000

    remove unnecessary variable deletion

--- modules/terraform-aws-ca-lambda/lambda_code/tls_cert/tls_cert.py
+++ modules/terraform-aws-ca-lambda/lambda_code/tls_cert/tls_cert.py
@@ -3,7 +3,6 @@ import os
 from utils.certs.kms_ca import kms_ca_generate_key_pair
 from utils.certs.kms import kms_get_kms_key_id, kms_describe_key
 from utils.certs.crypto import (
-    clear_variables,
     crypto_cert_request_info,
     crypto_encode_private_key,
     crypto_cert_info,
@@ -121,9 +120,6 @@ def create_csr_info(common_name, locality=None, organization=None, organizationa
 
 
 def lambda_handler(event, context):  # pylint:disable=unused-argument,disable=too-many-locals
-
-    # clear variables from memory to prevent reuse between successive lambda invocations
-    clear_variables()
 
     # get Issuing CA name
     issuing_ca_name = ca_name("issuing")
