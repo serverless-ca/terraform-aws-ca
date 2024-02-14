5bb26317a1fbc4bd920740404c0d124fa574d762
Paul Schwarzenberger <paul@celidor.net>
2024-02-14 22:55:13 +0000

    Terraform fmt

--- modules/terraform-aws-ca-step-function/locals.tf
+++ modules/terraform-aws-ca-step-function/locals.tf
@@ -1,3 +1,3 @@
 locals {
-    template_name_prefix = contains(var.cert_info_files, "tls") ? "ca" : "ca-no-gitops"
+  template_name_prefix = contains(var.cert_info_files, "tls") ? "ca" : "ca-no-gitops"
 }
