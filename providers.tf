e2ce68ef6db1bb661ffeca4e5f2934924bfd035a
Paul Schwarzenberger <paul@celidor.net>
2024-02-29 12:00:17 +0000

    us-east-1 provider only in module (#25)

--- providers.tf
+++ providers.tf
@@ -1,9 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 5.0"
-      configuration_aliases = [aws, aws.us-east-1]
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
     }
   }
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
 }
