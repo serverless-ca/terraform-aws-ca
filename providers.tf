16f7a898d2489e66f25bfec776d4a751d2b06618
Paul Schwarzenberger <paul@celidor.net>
2024-02-29 13:14:45 +0000

    Revert "us-east-1 provider only in module (#25)"
    This reverts commit e2ce68ef6db1bb661ffeca4e5f2934924bfd035a.

--- providers.tf
+++ providers.tf
@@ -1,13 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
+      source                = "hashicorp/aws"
+      version               = ">= 5.0"
+      configuration_aliases = [aws, aws.us-east-1]
     }
   }
-}
-
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
 }
