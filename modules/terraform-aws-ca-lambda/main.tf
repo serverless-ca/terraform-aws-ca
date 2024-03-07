2f4a5c43a981e489256b07a67be377fabbb6e964
new23d <dhruvahuja@gmail.com>
2024-03-07 11:56:38 +0000

    intermediate source code archive to check for changes (#30)
    * intermediate source code archive to check for changes
    * introduce caching of built python modules
    * arbitrary change to lambda source code for testing cache keys in the pipeline
    * giving var a proper name hoping to pass static analysis
    * moved python builds to "data" type (from "resource")
    * build when build files are absent
    * adding cache to rsa public crl workflow as well, and update cache action version

--- modules/terraform-aws-ca-lambda/main.tf
+++ modules/terraform-aws-ca-lambda/main.tf
@@ -1,6 +1,9 @@
 resource "null_resource" "install_python_dependencies" {
   triggers = {
-    always_run = timestamp()
+    source_archive_checksum = data.archive_file.lambda_source.output_base64sha256
+
+    # static value (true) if present, variable value (timestamp()) when not present. (so the 'false' state isn't static and forces a build by change of state whenever so. a static false value doesn't force change of state.)
+    build_already_present = fileexists("${path.module}/build/${local.file_name}/__init__.py") ? true : timestamp()
   }
 
   provisioner "local-exec" {
@@ -19,16 +22,22 @@ resource "null_resource" "install_python_dependencies" {
   }
 }
 
+data "archive_file" "lambda_source" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_code/${local.file_name}"
+  output_path = "${path.module}/archive/${local.file_name}.src.zip"
+}
+
 data "archive_file" "lambda_zip" {
   depends_on  = [null_resource.install_python_dependencies]
   type        = "zip"
   source_dir  = "${path.module}/build/${local.file_name}"
-  output_path = "${path.module}/build/${local.file_name}.zip"
+  output_path = "${path.module}/archive/${local.file_name}.zip"
 }
 
 resource "aws_lambda_function" "lambda" {
-  filename         = "${path.module}/build/${local.file_name}.zip"
-  source_code_hash = sha1(join("", [for f in fileset("${path.module}/lambda_code/${local.file_name}", "*") : filesha1("${path.module}/lambda_code/${local.file_name}/${f}")]))
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   function_name    = "${var.project}-${var.function_name}-${var.env}"
   description      = "${var.project} ${var.description}"
   role             = var.lambda_role_arn
