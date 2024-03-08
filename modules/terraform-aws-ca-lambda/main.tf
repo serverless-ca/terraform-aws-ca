f3463a22289cee320d98e90cb35d9fde460a3a85
Paul Schwarzenberger <paul@celidor.net>
2024-03-08 15:26:43 +0000

    Address PR comments

--- modules/terraform-aws-ca-lambda/main.tf
+++ modules/terraform-aws-ca-lambda/main.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "install_python_dependencies" {
   triggers = {
     # detect changes to Lambda code
-    lambda_code_sha256 = sha256(join("", sort([for f in fileset("${path.module}/lambda_code/${local.file_name}", "**") : filesha1("${path.module}/lambda_code/${local.file_name}/${f}")])))
+    lambda_code_sha256 = sha256(join("", [for f in toset(sort(tolist(fileset("${path.module}/lambda_code/${local.file_name}", "**")))) : filesha256("${path.module}/lambda_code/${local.file_name}/${f}")]))
 
     # detect changes to files in utils directory
-    utils_sha256 = sha256(join("", sort([for f in fileset("${path.module}/utils", "**") : filesha1("${path.module}/utils/${f}")])))
+    utils_sha256 = sha256(join("", [for f in toset(sort(tolist(fileset("${path.module}/utils", "**")))) : filesha256("${path.module}/utils/${f}")]))
 
     # static value (true) if present, variable value (timestamp()) when not present. (so the 'false' state isn't static and forces a build by change of state whenever so. a static false value doesn't force change of state.)
     build_already_present = fileexists("${path.module}/build/${local.file_name}/__init__.py") ? true : timestamp()
