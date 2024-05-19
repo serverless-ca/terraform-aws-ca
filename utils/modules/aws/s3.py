d09d7ea23e627cfe0c439890906e620ac4381a88
Paul Schwarzenberger <paul@celidor.net>
2024-05-19 11:59:56 +0100

    Add CRL integration test

--- utils/modules/aws/s3.py
+++ utils/modules/aws/s3.py
@@ -11,9 +11,33 @@ def get_s3_bucket(bucket_purpose="internal"):
     return [b["Name"] for b in s3_buckets if f"-{bucket_purpose}-" in b["Name"]][0]
 
 
+def list_s3_object_keys(bucket_name):
+    """
+    List object keys in S3 bucket
+    """
+
+    s3_client = boto3.client("s3")
+    response = s3_client.list_objects_v2(Bucket=bucket_name)
+
+    s3_objects = response["Contents"]
+
+    return [s3_object["Key"] for s3_object in s3_objects]
+
+
+def get_s3_object(bucket_name, key):
+    """
+    Get object from S3
+    """
+
+    s3_client = boto3.client("s3")
+    response = s3_client.get_object(Bucket=bucket_name, Key=key)
+
+    return response["Body"].read()
+
+
 def put_s3_object(bucket_name, kms_arn, key, data, encryption_algorithm="aws:kms"):
     """
-    Put an object in an S3 bucket
+    Put object in S3 bucket
     """
 
     s3_client = boto3.client("s3")
