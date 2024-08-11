d7f17a815073a0384196bc05d58dce923e022184
Paul Schwarzenberger <paul@celidor.net>
2024-08-11 14:50:21 +0100

    SNS topic

---
+++ modules/terraform-aws-ca-sns/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  sns_topic_name = coalesce(var.custom_sns_topic_name, "${var.project}}-${var.function}-${var.env}")
+
+  tags = merge(var.tags, {
+    Terraform = "true"
+    Name      = local.sns_topic_name,
+  })
+}
