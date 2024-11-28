9f182810a8cc52f7f96371bd5521fade469e97d5
Boris Gorbuntsov <boris.gorbuntsov@tytocare.com>
2024-11-29 01:31:24 +0200

    AWS session processing added. File and directory names adapted

--- utils/client-cert.py
+++ utils/client-cert.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import json
 import base64
-import boto3
 import os
+import sys
+import argparse
+import boto3
 from cryptography.hazmat.primitives.serialization import load_der_private_key
 from modules.certs.crypto import create_csr_info, crypto_encode_private_key, crypto_tls_cert_signing_request
 from modules.certs.kms import kms_generate_key_pair, kms_get_kms_key_id
@@ -10,35 +12,89 @@ from modules.aws.lambdas import get_lambda_name
 
 # identify home directory and create certs subdirectory if needed
 homedir = os.path.expanduser("~")
-base_path = f"{homedir}/certs"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+templatesdir = f'{dir_path}/templates'
+base_path = f"{homedir}/ca/client_certificates"
+client_template = f'{templatesdir}/client_certificate_variables.json'
 if not os.path.exists(base_path):
     print(f"Creating directory {base_path}")
     os.makedirs(base_path)
 
+def parse_variables(input_file):
+    """
+    Parse template file to obtain variables
+    """
+
+    if os.path.isfile(input_file):
+        try:
+            with open(input_file, encoding="utf-8") as json_file:
+                data = json.load(json_file)
+        except Exception as e:
+            data = {"error": f'Unable to parse JSON. {e}'}
+    else:
+        data = {"error": "No file"}
+    return data
+
+def get_session(aws_profile):
+    """
+    Open boto3 connection using profile
+    """
+
+    try:
+        aws_session = boto3.session.Session(profile_name=aws_profile)
+    except Exception as e:
+        aws_session = {"error": e}
+    return aws_session
 
 def main():  # pylint:disable=too-many-locals
     """
     Create test client certificate for default Serverless CA environment
     """
 
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--profile", help="AWS profile described in .aws/config file")
+    args = parser.parse_args()
+    profile = vars(args)["profile"]
+    if profile is None:
+        profile_name = "default"
+    else:
+        profile_name = profile
+
+    # create AWS session
+    session = get_session(profile)
+    if isinstance(session, dict):
+        print(f'Error: Unable to open session using profile {profile_name}. Error reported is: {session["error"]}')
+    else:
+        print(f'AWS session opened using profile: {profile_name}')
+
     # set variables
-    lifetime = 90
-    common_name = "My Test Certificate"
-    country = "GB"
-    locality = "London"
-    state = "England"
-    organization = "Serverless Inc"
-    organizational_unit = "Security Operations"
-    purposes = ["client_auth"]
+    variables = parse_variables(client_template)
+    if "error" in variables:
+        print(f'Error: Unable to read variables. Error reported is: {variables["error"]}')
+    else:
+        print(f'Variables are obtained from file {client_template}')
+
+    lifetime = variables["lifetime"]
+    common_name = variables["common_name"]
+    country = variables["country"]
+    locality = variables["locality"]
+    state = variables["state"]
+    organization = variables["organization"]
+    organizational_unit = variables["organizational_unit"]
+    purposes = variables["purposes"]
     output_path_cert_key = f"{base_path}/client-key.pem"
-    output_path_cert_pem = f"{base_path}/client-cert.pem"
+    output_path_cert_pem = f"{base_path}/issuer-bundle.pem"
     output_path_cert_crt = f"{base_path}/client-cert.crt"
-    output_path_cert_combined = f"{base_path}/client-cert-key.pem"
-    key_alias = "serverless-tls-keygen-dev"
+    output_path_cert_combined = f"{base_path}/ca-bundle.pem"
+    key_alias = variables["key_alias"]
 
     # create key pair using symmetric KMS key to provide entropy
-    key_id = kms_get_kms_key_id(key_alias)
-    kms_response = kms_generate_key_pair(key_id)
+    key_id = kms_get_kms_key_id(key_alias, session=session)
+    if isinstance(key_id, dict):
+        print(f'Error: {key_id["error"]}')
+        sys.exit(1)
+    kms_response = kms_generate_key_pair(key_id, session=session)
     private_key = load_der_private_key(kms_response["PrivateKeyPlaintext"], None)
 
     # create CSR
@@ -58,8 +114,8 @@ def main():  # pylint:disable=too-many-locals
     request_payload_bytes = json.dumps(request_payload)
 
     # Invoke TLS certificate Lambda function
-    lambda_name = get_lambda_name("tls-cert")
-    client = boto3.client("lambda")
+    lambda_name = get_lambda_name("tls-cert", session=session)
+    client = session.client("lambda")
     response = client.invoke(
         FunctionName=lambda_name,
         InvocationType="RequestResponse",
@@ -87,7 +143,7 @@ def main():  # pylint:disable=too-many-locals
     if output_path_cert_pem:
         with open(output_path_cert_pem, "w", encoding="utf-8") as f:
             f.write(cert_data.decode("utf-8"))
-            print(f"Certificate written to {output_path_cert_pem}")
+            print(f"Intermediate bundle written to {output_path_cert_pem}")
 
     if output_path_cert_crt:
         with open(output_path_cert_crt, "w", encoding="utf-8") as f:
@@ -98,8 +154,7 @@ def main():  # pylint:disable=too-many-locals
         with open(output_path_cert_combined, "w", encoding="utf-8") as f:
             f.write(key_data.decode("utf-8"))
             f.write(cert_data.decode("utf-8"))
-
-    print(f"Certificate and key written to {output_path_cert_combined}")
+            print(f"Combined root and intermediate bundle written to {output_path_cert_combined}")
 
 
 if __name__ == "__main__":
