0abf35b1ed09881a07a9e10d1837d98dc7454a5f
Boris Gorbuntsov <boris.gorbuntsov@tytocare.com>
2025-04-02 13:04:56 +0300

    Optional xray permissions policy attachement added. Enabling active tracing variable added

--- modules/terraform-aws-ca-iam/main.tf
+++ modules/terraform-aws-ca-iam/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda" {
-  name = "${var.project}-${var.function_name}-${var.env}"
+  name               = "${var.project}-${var.function_name}-${var.env}"
   assume_role_policy = templatefile("${path.module}/templates/${var.assume_role_policy}_role.json.tpl", {
     aws_principals = var.aws_principals
     project        = var.project
@@ -7,8 +7,8 @@ resource "aws_iam_role" "lambda" {
 }
 
 resource "aws_iam_role_policy" "lambda" {
-  name = "${var.project}-${var.function_name}-${var.env}"
-  role = aws_iam_role.lambda.id
+  name   = "${var.project}-${var.function_name}-${var.env}"
+  role   = aws_iam_role.lambda.id
   policy = templatefile("${path.module}/templates/${var.policy}_policy.json.tpl", {
     kms_arn_issuing_ca     = var.kms_arn_issuing_ca,
     kms_arn_root_ca        = var.kms_arn_root_ca,
@@ -19,4 +19,10 @@ resource "aws_iam_role_policy" "lambda" {
     internal_s3_bucket_arn = var.internal_s3_bucket_arn
     sns_topic_arn          = var.sns_topic_arn
   })
+}
+
+resource "aws_iam_role_policy_attachment" "xray" {
+  count      = var.xray_enabled ? 1 : 0
+  role       = aws_iam_role.lambda.name
+  policy_arn = var.xray_daemon_policy_arn
 }
