71f6fa113403c5bb8cb342884992d2a89e2787d4
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2026-05-29 15:36:51 +0100

    Rename new locals

--- locals.tf
+++ locals.tf
@@ -1,11 +1,11 @@
 locals {
   runtime = coalesce(var.runtime, format("python%s", regex("^\\d+\\.\\d+", file("${path.module}/python-version"))))
 
-  create_root_ca_lambda_name        = "create-root-ca"
-  create_rsa_issuing_ca_lambda_name = "create-issuing-ca"
-  rsa_root_ca_crl_lambda_name       = "root-ca-crl"
-  rsa_issuing_ca_crl_lambda_name    = "issuing-ca-crl"
-  rsa_tls_cert_lambda_name          = "tls-cert"
-  expiry_lambda_name                = "expiry"
-  notify_lambda_name                = "notify"
+  create_root_ca_function_name    = "create-root-ca"
+  create_issuing_ca_function_name = "create-issuing-ca"
+  root_ca_crl_function_name       = "root-ca-crl"
+  issuing_ca_crl_function_name    = "issuing-ca-crl"
+  tls_cert_function_name          = "tls-cert"
+  expiry_function_name            = "expiry"
+  notify_function_name            = "notify"
 }
