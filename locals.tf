22acb61517af9ccc48b2e2ff3ead98f3ec4a5f8c
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2026-05-29 16:15:55 +0100

    Fix Lambda/IAM Role/Log Group name mismatches (#605)
    Fixes mismatches between Lambda Function names, IAM Role names, and the allowed Log Group destinations for the Lambda Execution Roles.
    
    This change replaces the `issuing_crl_iam` and `root_crl_iam` IAM Roles. In both cases, the Lambda Function names contain "-ca" whereas the IAM Role names and permitted Log Group resources names do not.

--- locals.tf
+++ locals.tf
@@ -1,3 +1,11 @@
 locals {
   runtime = coalesce(var.runtime, format("python%s", regex("^\\d+\\.\\d+", file("${path.module}/python-version"))))
+
+  create_root_ca_function_name    = "create-root-ca"
+  create_issuing_ca_function_name = "create-issuing-ca"
+  root_ca_crl_function_name       = "root-ca-crl"
+  issuing_ca_crl_function_name    = "issuing-ca-crl"
+  tls_cert_function_name          = "tls-cert"
+  expiry_function_name            = "expiry"
+  notify_function_name            = "notify"
 }
